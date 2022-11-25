ce06e954b8def64236468ac9b2eb404a6bab3030
Alexander Jacob <alexander.jacob@jacob -consulting.de>
2022-11-25 10:37:34 +0100

    works with schema foo

--- issue/models.py
+++ issue/models.py
@@ -5,7 +5,7 @@ from .base import Base
 
 class CustomerType(Base):
     __tablename__ = "customer_type"
-    __table_args__ = {"schema": "public"}
+    __table_args__ = {"schema": "foo"}
 
     id = Column(Integer, primary_key=True)
     code = Column(String(32), comment="customer type code", nullable=False)
@@ -13,9 +13,9 @@ class CustomerType(Base):
 
 class Customer(Base):
     __tablename__ = "customer"
-    __table_args__ = {"schema": "public"}
+    __table_args__ = {"schema": "foo"}
 
     primary_key = Column(String(32), primary_key=True, comment="UID")
     first_name = Column(String(100), comment="First name", nullable=False)
     last_name = Column(String(100), comment="Last name", nullable=False)
-    customer_type_id = Column(Integer, ForeignKey("public.customer_type.id"))
+    customer_type_id = Column(Integer, ForeignKey("foo.customer_type.id"))
